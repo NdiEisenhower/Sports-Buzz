bd8b4723d3fe59b374b737cab5b58b95efd4f19e
Eisenhower <munei@immedia.co.za>
2019-08-07 21:37:46 +0200

    add new icons Handle smaller devices clean up

--- SportsBuzz/app/src/main/java/com/mmbadi/sportsbuzz/ui/MainActivity.java
+++ SportsBuzz/app/src/main/java/com/mmbadi/sportsbuzz/ui/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements ArticleViewAdapte
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        setTitle(getString(R.string.news));
         NETWORK_CURRENT = NetworkUtils.getNetworkStatus(this);
 
         articleRecyclerView = findViewById(R.id.sportsRecyclerView);
